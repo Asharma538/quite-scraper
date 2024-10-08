cdc9451e322b7c9b35079073001fe2bf46abf2cf
Anadi Sharma <sharma.130@iitj.ac.in>
2024-10-08 17:39:55 +0530

    Integrating Firebase with Instagram Scraper

--- app.go
+++ app.go
@@ -5,19 +5,24 @@ import (
 	"time"
 )
 
-func main(){
-	initFirebase();
-	Ig := Instagram{};
-	Ig.last_activity = make(map[string]int);
-	Ig.addUser("living__motivation");
-	
+func main() {
+	firebaseController := FirebaseController{}
+	firebaseController.collection = "Quite-Scraper"
+	firebaseController.init()
+	defer firebaseController.firestoreClient.Close()
+
+	Ig := Instagram{}
+	Ig.last_activity = make(map[string]int)
+	Ig.users_to_monitor = firebaseController.getUsersToMonitor("Instagram")
+
 	for {
-		for _,user := range Ig.users_to_monitor {
-			if Ig.checkAndUpdateActivity(user,Ig.last_activity[user]){
-				fmt.Println("New activity detected for user "+user);
+		for _, user := range Ig.users_to_monitor {
+			if Ig.checkAndUpdateActivity(user, Ig.last_activity[user]) {
+				fmt.Println("New activity detected for user " + user)
 			}
-			fmt.Println(user,Ig.last_activity[user]);
+			fmt.Println(user, Ig.last_activity[user])
 		}
-		time.Sleep(5*time.Minute);
+		fmt.Print("\n\n\n\n");
+		time.Sleep(5 * time.Minute)
 	}
 }
--- firebase.go
+++ firebase.go
@@ -3,24 +3,55 @@ package main
 import (
 	"context"
 	"fmt"
-	
+
+	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
-	
+
 	"google.golang.org/api/option"
 )
 
-func initFirebase() *auth.Client {
-	opt := option.WithCredentialsFile("./secret_key.json");
-	app, err := firebase.NewApp(context.Background(), nil, opt);
-	
+type FirebaseController struct {
+	authClient      *auth.Client
+	firestoreClient *firestore.Client
+	collection      string
+}
+
+func (controller *FirebaseController) init() {
+	opt := option.WithCredentialsFile("./secret_key.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+
 	if err != nil {
-		fmt.Printf("error initializing app: %v\n", err);
+		fmt.Printf("Error initializing app: %v\n", err)
 	}
-	
-	client, err := app.Auth(context.Background());
+
+	controller.authClient, err = app.Auth(context.Background())
 	if err != nil {
-		fmt.Printf("error getting Auth client: %v\n", err);
+		fmt.Printf("Error getting Auth client: %v\n", err)
 	}
-	return client;
+
+	controller.firestoreClient, err = app.Firestore(context.Background())
+	if err != nil {
+		fmt.Printf("Error initializing firestore client %v\n", err)
+	}
+
+	return
+}
+
+func (controller *FirebaseController) getUsersToMonitor(platform string) []string {
+	// Requesting the document of the Platform
+	snapshot, err := controller.firestoreClient.Collection(controller.collection).Doc(platform).Get(context.Background())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Getting the usernames array from the document
+	usernamesInterface := snapshot.Data()["usernames"].([]interface{})
+	usernames := make([]string, len(usernamesInterface))
+
+	for idx, user := range usernamesInterface {
+		usernames[idx] = user.(string)
+	}
+
+	return usernames
 }
