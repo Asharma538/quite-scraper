fd483461855701db0ec8d540ea7fc9018e2b71e0
Anadi Sharma <sharma.130@iitj.ac.in>
2024-10-11 03:47:32 +0530

    Changing the response structure

--- backend/app.go
+++ backend/app.go
@@ -19,23 +19,39 @@ func main() {
 	Ig.Last_activity = make(map[string]int)
 	Ig.Users_to_monitor = firebaseController.GetUsersToMonitor("Instagram")
 
+	var waitGroup sync.WaitGroup
+	for _, user := range Ig.Users_to_monitor {
+		waitGroup.Add(1)
+		go func(u string) {
+			defer waitGroup.Done()
+			Ig.CheckAndUpdateActivity(u, Ig.Last_activity[u])
+		}(user)
+	}
+	waitGroup.Wait()
+
 	fmt.Println("Users to monitor: ", Ig.Users_to_monitor)
 
 	http.HandleFunc("/getactivity", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
-		var waitGroup sync.WaitGroup
+		var users_with_updates []string;
+		var mutex sync.Mutex
+
 		for _, user := range Ig.Users_to_monitor {
 			waitGroup.Add(1)
 			go func(u string) {
 				defer waitGroup.Done()
-				Ig.CheckAndUpdateActivity(u, Ig.Last_activity[u])
+				if Ig.CheckAndUpdateActivity(u, Ig.Last_activity[u]) {
+					mutex.Lock()
+					users_with_updates = append(users_with_updates, u);
+					mutex.Unlock()
+				}
 			}(user)
 		}
 		waitGroup.Wait()
-
-		responseJson, err := json.Marshal(Ig.Last_activity)
+		
+		responseJson, err := json.Marshal(map[string][]string{"users_with_updates": users_with_updates})
 		if err != nil {
 			fmt.Print("Error:" + err.Error())
 			return
